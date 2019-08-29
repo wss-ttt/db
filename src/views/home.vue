9f90a9ff845b96e87efbb1fda3e8825b72d1075a
王水胜 <602466011@qq.com>
2019-08-29 11:39:10 +0800

    更新:添加store>modules>home.js

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="home">home</div>
+  <div class="home">
+    <ul>
+      <li v-for="(item,index) in home.users" :key="index">
+        编号:{{item.id}}
+        <br>
+        姓名:{{item.name}}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import { mapState,mapActions} from 'vuex'
 export default {
   components: {},
   props: {},
@@ -10,10 +19,17 @@ export default {
     return {}
   },
   watch: {},
-  computed: {},
-  methods: {},
+  computed: {
+    ...mapState(['home'])
+  },
+  methods: {
+    ...mapActions(['loadMore']),   //  注意写法不要写错了
+  },
   created() {},
-  mounted() {}
+  mounted() {
+    // 执行该方法
+    this.loadMore()
+  }
 }
 </script>
 <style scoped>
