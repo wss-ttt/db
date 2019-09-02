58b871fbca1aeb7288242cd6c359f9ffe9fd33f1
王水胜 <602466011@qq.com>
2019-09-02 21:28:25 +0800

    更新：注册页面表单校验

--- src/views/register.vue
+++ src/views/register.vue
@@ -3,13 +3,37 @@
     <h3>赶紧注册吧~</h3>
     <form>
       <div class="form-item">
-        <input type="text" v-model="form.userName" class="input" placeholder="用户名" />
+        <label for>用户名</label>
+        <span class="error">{{userNameMsg}}</span>
+        <input
+          type="text"
+          v-model="form.userName"
+          @blur="checkInput('userName')"
+          class="input"
+          placeholder="用户名"
+        />
       </div>
       <div class="form-item">
-        <input type="password" v-model="form.pwd" class="input" placeholder="密码" />
+        <label for>密码</label>
+        <span class="error">{{pwdMsg}}</span>
+        <input
+          type="password"
+          v-model="form.pwd"
+          @blur="checkInput('pwd')"
+          class="input"
+          placeholder="密码"
+        />
       </div>
       <div class="form-item">
-        <input type="password" v-model="form.rePwd" class="input" placeholder="确认密码" />
+        <label for>确认密码</label>
+        <span class="error">{{rePwdMsg}}</span>
+        <input
+          type="password"
+          v-model="form.rePwd"
+          @blur="checkInput('rePwd')"
+          class="input"
+          placeholder="确认密码"
+        />
       </div>
       <div class="form-item">
         <button type="button" class="btn" @click="register">注册</button>
@@ -31,17 +55,30 @@ export default {
     return {
       form: {
         pwd: '', // 密码
-        rePwd:'',    // 确认密码
+        rePwd: '', // 确认密码
         userName: '' // 用户名
-      }
+      },
+      userNameMsg: '', // 用户名错误提示
+      pwdMsg: '', // 密码错误提示
+      rePwdMsg: '' // 确认密码错误提示
+      // isValid: false // 校验是否全部通过
     }
   },
   watch: {},
   computed: {
-    ...mapState(['user'])
+    ...mapState(['user']),
+    isValid() {
+      if (!this.userNameMsg && !this.pwdMsg && !this.rePwdMsg) return true
+      else false
+    }
   },
   methods: {
     register() {
+      // 在点击按钮前，也不需要做一次校验
+      this.checkInput('userName')
+      this.checkInput('pwd')
+      this.checkInput('rePwd')
+      if (!this.isValid) return // 校验没有通过,点击注册是没有反应的
       this.$http({
         url: 'http://localhost:2000/register',
         method: 'post',
@@ -58,15 +95,50 @@ export default {
         if (res.code === 0) {
           alert('注册成功')
           this.$store.commit({
-            type:'saveUser',
-            userName:this.form.userName,
-            pwd:this.form.pwd
+            type: 'saveUser',
+            userName: this.form.userName,
+            pwd: this.form.pwd
           })
           this.$router.push({
-            name:'home'
+            name: 'home'
           })
         }
       })
+    },
+    // 表单校验
+    checkInput(type) {
+      if (type === 'userName') {
+        if (this.form.userName === '') {
+          this.userNameMsg = '用户性不能为空'
+          return
+        }
+        this.userNameMsg = ''
+        console.log('userName')
+      }
+      if (type === 'pwd') {
+        if (this.form.pwd === '') {
+          this.pwdMsg = '密码不能为空'
+          return
+        }
+        if (this.form.pwd.length < 6) {
+          this.pwdMsg = '密码长度不能小于6位'
+          return
+        }
+        this.pwdMsg = ''
+        console.log('pwd')
+      }
+      if (type === 'rePwd') {
+        if (this.form.rePwd === '') {
+          this.rePwdMsg = '确认密码不能为空'
+          return
+        }
+        if (this.form.rePwd !== this.form.pwd) {
+          this.rePwdMsg = '两次密码不一样'
+          return
+        }
+        this.rePwdMsg = ''
+        console.log('确认密码')
+      }
     }
   },
   created() {},
@@ -84,6 +156,10 @@ export default {
   width: 100%;
   margin: 20px auto;
   position: relative;
+}
+.form-item label {
+  padding: 5px 0;
+  display: inline-block;
 }
 .form-item .input {
   width: 100%;
@@ -106,8 +182,12 @@ export default {
   cursor: pointer;
 }
 .tips {
-  text-align: center;
+  padding: 5px 0;
+  text-align: right;
   font-size: 12px;
   color: #ccc;
+}
+.error {
+  color: red;
 }
 </style>
